a1b370bd20a498dded36ea7cb238ede973cf48dc
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-06-09 09:32:35 +0500

    add

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram import F
 from data.config import BOT_TOKEN
+from inline_buttons import calc_btn
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
@@ -12,7 +13,12 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     full_name = message.from_user.full_name
-    await message.answer(f"Salom, {full_name}")
+    await message.answer(f"Salom, {full_name}, \nMen oddiy kalkulyatorman",reply_markup=calc_btn.as_markup())
+
+@dp.callback_query()
+async def calc_query(callback: types.CallbackQuery):
+    text = f"{callback.data} bosildi!"
+    await callback.answer(text=text)
 
 
 async def main():
