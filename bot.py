e67af154aa9c68ad49f3238c901d1810eec8660c
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-06-10 09:42:48 +0500

    calc

--- bot.py
+++ bot.py
@@ -13,12 +13,40 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     full_name = message.from_user.full_name
-    await message.answer(f"Salom, {full_name}, \nMen oddiy kalkulyatorman",reply_markup=calc_btn.as_markup())
+    await message.answer(f"Salom, {full_name}, \nMen oddiy kalkulyatorman")
+    await message.answer("0",reply_markup=calc_btn.as_markup())
 
 @dp.callback_query()
 async def calc_query(callback: types.CallbackQuery):
-    text = f"{callback.data} bosildi!"
-    await callback.answer(text=text)
+    text = callback.message.text
+    data = callback.data
+    if data.isdigit():
+        if callback.message.text == "0":
+            text = data
+        else:
+            text +=data
+    if data == "D":
+        if len(callback.message.text)== 1:
+            text = "0"
+        else:
+            text = text[:-1]
+    if data =="C":
+        text = "0"
+    if data in "+x/-":
+        if not text[-1].isdigit():
+            await callback.answer("Notog'ri amal bajardingiz!!!")
+        else:
+            text += data
+    if data == "=":
+        
+        text = str(eval(text.replace("x","*")))
+
+    try:
+        await callback.message.edit_text(text=text,reply_markup=calc_btn.as_markup())
+    except:
+        pass
+    
+    # await callback.answer(text=text)
 
 
 async def main():
--- inline_buttons.py
+++ inline_buttons.py
@@ -1,6 +1,7 @@
-buttons = "123x456-789/0.+="
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+buttons = "D()x789/456-123+C0.="
 calc_btn = InlineKeyboardBuilder()
 for i in buttons:
     calc_btn.add(InlineKeyboardButton(text=i,callback_data=i))
